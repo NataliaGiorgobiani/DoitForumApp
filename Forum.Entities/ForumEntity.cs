272fd46ce0dd2be0eae46155a16611cb1d1d0fa5
NataliaGiorgobiani <natalia.giorgobiani1@gmail.com>
2024-05-20 15:17:10 +0400

    1

--- Forum.Entities/ForumEntity.cs
+++ Forum.Entities/ForumEntity.cs
@@ -1,7 +1,13 @@
-﻿namespace Forum.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Forum.Entities
 {
     public class ForumEntity
     {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
     }
 }
