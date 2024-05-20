0218008aac21d0724af3895fc2506b515ed52374
NataliaGiorgobiani <natalia.giorgobiani1@gmail.com>
2024-05-20 15:35:21 +0400

    2

--- Forum.Entities/ForumEntity.cs
+++ Forum.Entities/ForumEntity.cs
@@ -9,5 +9,29 @@ namespace Forum.Entities
         [Required]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string User { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+
+
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
     }
 }
