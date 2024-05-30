804deeb9549aafa090ee6d0f6fb1d7808a1d5c95
NataliaGiorgobiani <natalia.giorgobiani1@gmail.com>
2024-05-30 17:01:32 +0400

    111

---
+++ Forum/ForumAPI/MiddlwareExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Forum.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumAPI
+{
+    public static class MiddlwareExtensions
+    {
+        public static void AddDBContext(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SQLServerLocalConnection")));
+        }
+        public static void AddControllers(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+        }
+
+        public static void AddEndpointsApiExplorer(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddEndpointsApiExplorer();
+        }
+
+        public static void AddSwaggerGen(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddSwaggerGen();
+        }
+    }
+}
--- Forum/ForumAPI/Program.cs
+++ Forum/ForumAPI/Program.cs
@@ -8,11 +8,10 @@ namespace ForumAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.AddDBContext();
+            builder.AddControllers();
+            builder.AddEndpointsApiExplorer();
+            builder.AddSwaggerGen();
 
             var app = builder.Build();
 
